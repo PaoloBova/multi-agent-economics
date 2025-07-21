04429c2423b15258e7cb1f66f6cea5540c2edc72
Paolo Bova <paolobova@protonmail.com>
2025-07-21 22:36:35 +0100

    fixup!

--- multi_agent_economics/agents/__init__.py
+++ multi_agent_economics/agents/__init__.py
@@ -8,11 +8,3 @@ in simulations, including traders, consumers, producers, and market makers.
 from .economic_agent import EconomicAgent, create_agent
 
 __all__ = ["EconomicAgent", "create_agent"]
-from .producer import ProducerAgent
-
-__all__ = [
-    "BaseAgent",
-    "TraderAgent", 
-    "ConsumerAgent",
-    "ProducerAgent",
-]
